81959daafa01ee4dce09fa4666c10f20984f9aa4
dangnhon <dang.nhon12345@yahoo.com>
2021-01-13 15:45:35 -0800

    hold the stretch goals

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// document.addEventListener("DOMContentLoaded", () => {
+
+// });
+
+
+// global variables:
+const form = document.getElementById('create-task-form')
+
+
+// handler functions: 
+function handleSubmit(e) {
+  e.preventDefault() 
+  const userInput = e.target["new-task-description"].value
+  displayUserInput(userInput)
+  form.reset()
+}
+
+
+// display functions: 
+function displayUserInput(input) {
+  const taskUL = document.getElementById('tasks')
+  const taskList = document.createElement('li')
+  taskList.innerText = input
+  taskUL.appendChild(taskList)
+
+}
+
+
+// event listeners:
+form.addEventListener("submit", handleSubmit) 
